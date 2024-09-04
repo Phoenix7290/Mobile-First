6cd8a97c52cd101de40f17cac35ec7c6ad38b24f
Phoenix7290 <b5261064@gmail.com>
2024-09-04 16:07:34 -0300

    Correção do CSS

--- src/TP2/16/components/VideoPlayer.jsx
+++ src/TP2/16/components/VideoPlayer.jsx
@@ -3,7 +3,7 @@ import './styles.css';
 const VideoPlayer = () => {
   return (
     <div className="video-player">
-      <iframe src="https://www.youtube.com/embed/MJbE3uWN9vE?si=9k0WmTiwv0gOZQXz&amp;start=55" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <iframe src="https://www.youtube.com/embed/MJbE3uWN9vE?si=9k0WmTiwv0gOZQXz&amp;start=55" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
     </div>
   );
 };
